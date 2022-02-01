b3b65d5c51e3d6f414da03c2a1c9c5ebd59eaa73
kenshirou01 <rush2065kingcrab0120@gmail.com>
2022-02-02 01:58:46 +0900

    Room 登録 一覧表示
    登録,一覧表示機能の作成（画像未実装）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,8 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :profile, :image)
-  end
+    def user_params
+      params.require(:user).permit(:name, :profile, :image)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'users/show'
-  get 'home/index'
+  get 'rooms/index'
   root to: 'users#index'
   resources :users
   resources :rooms
